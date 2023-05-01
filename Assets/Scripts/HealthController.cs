a26fef9e4a36b2f55f48ef80fddb216096f0937b
kromek7 <116305278+kromek7@users.noreply.github.com>
2023-05-01 11:03:53 +0200

    HEALTHSYSTEM

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public int playerHealth = 3;
+    [SerializeField] private Image[] hearts;
+    [SerializeField] private DamageController _damageController;
+
+    private void Start()
+    {
+        UpdateHealth();
+    }
+
+    public void UpdateHealth()
+    {
+        if (playerHealth <= 0) // Kiedy postaæ straci wszystkie ¿ycia.
+        {
+            //Restart the game
+            //or
+            //Freeze the game etc
+
+            //this.gameObject.GetComponent<skryptdosterowania>.enabled = false;
+
+            Debug.Log("GAME OVER");
+        }
+
+
+        {
+            for (int i = 0; i < hearts.Length; i++)
+            {
+                if (i < playerHealth)
+                {
+                    hearts[i].color = Color.red;
+                }
+                else
+                {
+                    hearts[i].color = Color.black;
+                }
+            }
+        }
+    }
+}
